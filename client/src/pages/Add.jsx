3ac791073aea7e029a35e4f9ed3751fb2a746caf
Rochelle <113217202+rochelle-al@users.noreply.github.com>
2023-10-14 11:39:34 +1100

    Update Add.jsx

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -4,41 +4,48 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Add = () => {
-    const [book,setBook] = useState({
-        title:"",
-        desc:"",
-        price:"null",
-        cover:"",
+    // State to store book information
+    const [book, setBook] = useState({
+        title: "",
+        desc: "",
+        price: "null",
+        cover: "",
     });
 
+    // React Router's navigate function to redirect to different routes
     const navigate = useNavigate()
 
+    // Function to handle input changes and update the book state
     const handleChange = (e) => {
         setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
     };
 
-    const handleClick = async e =>{
-        e.preventDefault()
-        try{
-          await axios.post("http://localhost:8800/books", book)
-          navigate("/")
-        }catch(err){
-            console.log(err)
-
+    // Function to handle the "Add" button click event
+    const handleClick = async e => {
+        e.preventDefault(); // Prevent the default form submission behavior
+        try {
+            // Send a POST request to add a new book
+            await axios.post("http://localhost:8800/books", book);
+            // Redirect to the home page after successfully adding the book
+            navigate("/");
+        } catch (err) {
+            console.log(err); // Log any errors to the console
         }
     }
 
-    console.log(book)
+    console.log(book); // Log the current book state to the console
     return (
         <div className='form'>
             <h1>ADD NEW BOOK</h1>
-            <input type="text" placeholder='title' onChange = {handleChange} name="title"/>
-            <input type="text" placeholder='desc' onChange = {handleChange} name="desc"/>
-            <input type="number" placeholder='price' onChange = {handleChange} name="price"/>
-            <input type="text" placeholder='cover' onChange = {handleChange} name="cover"/>
+            {/* Input fields for title, desc, price, and cover */}
+            <input type="text" placeholder='title' onChange={handleChange} name="title" />
+            <input type="text" placeholder='desc' onChange={handleChange} name="desc" />
+            <input type="number" placeholder='price' onChange={handleChange} name="price" />
+            <input type="text" placeholder='cover' onChange={handleChange} name="cover" />
+            {/* "Add" button with a click event handler */}
             <button className="formButton" onClick={handleClick}>Add</button>
         </div>
     )
 }
 
-export default Add
+export default Add;
